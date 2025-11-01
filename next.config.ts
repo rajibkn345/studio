635d1819f99fa0f0514a7b838fdd1b8e16f6ad55
Chip Joyner <chip@joynerventures.com>
2025-11-01 20:57:42 +0000

    There are still mistakes and image and word bleed.  Before trying to cor

---
+++ next.config.ts
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'storage.googleapis.com' },
+      { protocol: 'https', hostname: 'images.unsplash.com' },
+      { protocol: 'https', hostname: 'cdn.worldvectorlogo.com' },
+    ],
+  },
+};
+
+module.exports = nextConfig;
