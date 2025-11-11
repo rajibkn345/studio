aa6ace2662b295a80f91f91c68525b0f82c6491c
Chip Joyner <chip@joynerventures.com>
2025-11-11 21:51:58 +0000

    Add to brand logos in the "About" section

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,7 @@ const nextConfig = {
       { protocol: 'https', hostname: 'images.unsplash.com' },
       { protocol: 'https', hostname: 'cdn.worldvectorlogo.com' },
       { protocol: 'https', hostname: 'picsum.photos' },
+      { protocol: 'https', hostname: 'images.squarespace-cdn.com' },
     ],
   },
 };
